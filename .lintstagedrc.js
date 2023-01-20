231d88906876222d91b4f7e644614c35590dd060
NiftyAndy <andy@niftyleague.com>
2023-01-20 19:33:43 -0400

    Setup husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // Type check TypeScript files
+  // TODO: fix typescript issues and enable
+  // '**/*.ts': () => 'yarn tsc --noEmit',
+
+  // Lint then format TypeScript and JavaScript files
+  '**/*.(ts|js)': filenames => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
+
+  // Lint then formatSolidity
+  '**/*.sol': filenames => [
+    // TODO: fix solidity compiler issues and enable
+    // `yarn solhint ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+};
