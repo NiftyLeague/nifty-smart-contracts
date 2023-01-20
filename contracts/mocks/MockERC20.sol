455fe0e08df8cdcf534b5900a16a0ff01126a105
0xKey <davidlee152025@gmail.com>
2023-01-20 21:05:14 +0900

    update MockERC20 contract

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract MockERC20 is ERC20 {
-  constructor() ERC20("MockToken", "TESTTOKEN") {}
+contract MockERC20 is ERC20PresetMinterPauser {
+  constructor() ERC20PresetMinterPauser("MockToken", "TESTTOKEN") {}
 
-  function mint(address to, uint256 amount) public {
+  function mint(address to, uint256 amount) public override {
     _mint(to, amount);
   }
 }
