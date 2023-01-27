f4bdb499e0e486752fdbae4127571db2e62dced5
0xKey <davidlee152025@gmail.com>
2023-01-27 22:02:16 +0900

    add the tests for NiftyRareDegenDistribution contract

--- test/NiftyRareDegenDistribution.ts
+++ test/NiftyRareDegenDistribution.ts
@@ -44,6 +44,16 @@ describe('NiftyRareDegenDistribution', function () {
     }
   });
 
+  describe('Initialize', () => {
+    it('Should be able to initialize the contract', async () => {
+      const NiftyRareDegenDistribution = await ethers.getContractFactory('NiftyRareDegenDistribution');
+      niftyRareDegenDistribution = (await upgrades.deployProxy(NiftyRareDegenDistribution, [
+        niftyDegen.address,
+        niftyWallet.address,
+      ])) as NiftyRareDegenDistribution;
+    });
+  });
+
   describe('updateNiftyDegen', () => {
     it('Should be able to update the NiftyDegen address', async () => {
       expect(await niftyRareDegenDistribution.niftyDegen()).to.equal(niftyDegen.address);
@@ -52,6 +62,11 @@ describe('NiftyRareDegenDistribution', function () {
       await niftyRareDegenDistribution.updateNiftyDegen(bob.address);
 
       expect(await niftyRareDegenDistribution.niftyDegen()).to.equal(bob.address);
+    });
+
+    it('Should revert if the caller is not the owner', async () => {
+      // set the NiftyDegen address
+      await expect(niftyRareDegenDistribution.connect(alice).updateNiftyDegen(bob.address)).to.be.reverted;
     });
   });
 
@@ -63,6 +78,11 @@ describe('NiftyRareDegenDistribution', function () {
       await niftyRareDegenDistribution.updateNiftyWallet(bob.address);
 
       expect(await niftyRareDegenDistribution.niftyWallet()).to.equal(bob.address);
+    });
+
+    it('Should revert if the caller is not the owner', async () => {
+      // set the nifty wallet address
+      await expect(niftyRareDegenDistribution.connect(alice).updateNiftyWallet(bob.address)).to.be.reverted;
     });
   });
 
@@ -79,9 +99,17 @@ describe('NiftyRareDegenDistribution', function () {
 
       expect(await niftyRareDegenDistribution.getRareDegensCount()).to.equal(rareDegenCount);
     });
+
+    it('Should revert if the caller is not the owner', async () => {
+      const rareDegenTokenIds = [28, 29, 30, 31, 32, 33, 34, 35, 36, 37];
+
+      // deposit the rare degens
+      await niftyDegen.connect(alice).setApprovalForAll(niftyRareDegenDistribution.address, true);
+      await expect(niftyRareDegenDistribution.connect(alice).depositRareDegens(rareDegenTokenIds)).to.be.reverted;
+    });
   });
 
-  describe.skip('claimRandomRareDegen', () => {
+  describe('claimRandomRareDegen', () => {
     beforeEach(async () => {
       const rareDegenTokenIds = [28, 29, 30, 31, 32, 33, 34, 35, 36, 37];
 
@@ -91,21 +119,107 @@ describe('NiftyRareDegenDistribution', function () {
     });
 
     it('Should be able to claim the rare degen', async () => {
+      // Alice claims the random rare degen, burn 8 normal degens to claim 1 rare degen
       let degenTokenIdList = [0, 1, 2, 3, 4, 5, 6, 7];
 
-      const aliceDegenCountBefore = await niftyDegen.balanceOf(alice.address);
-      const rareDegenCountBefore = await niftyRareDegenDistribution.getRareDegensCount();
+      let aliceDegenCountBefore = await niftyDegen.balanceOf(alice.address);
+      let rareDegenCountBefore = await niftyRareDegenDistribution.getRareDegensCount();
 
-      // Alice claims the random rare degen
       await niftyDegen.connect(alice).setApprovalForAll(niftyRareDegenDistribution.address, true);
       await niftyRareDegenDistribution.connect(alice).claimRandomRareDegen(degenTokenIdList);
 
-      // console.log(await niftyRareDegenDistribution.getRareDegenTokenIds());
+      let aliceDegenCountAfter = await niftyDegen.balanceOf(alice.address);
+      let rareDegenCountAfter = await niftyRareDegenDistribution.getRareDegensCount();
 
-      const aliceDegenCountAfter = await niftyDegen.balanceOf(alice.address);
-      const rareDegenCountAfter = await niftyRareDegenDistribution.getRareDegensCount();
+      expect(aliceDegenCountAfter).to.equal(aliceDegenCountBefore.sub(8).add(1));
+      expect(rareDegenCountAfter).to.equal(rareDegenCountBefore.sub(1));
 
-      expect(aliceDegenCountAfter).to.equal(aliceDegenCountBefore.sub(8));
+      // NiftyWallet claims the random rare degen, burn 12 normal degens to claim 1 rare degen
+      degenTokenIdList = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27];
+
+      let niftyWalletDegenCountBefore = await niftyDegen.balanceOf(niftyWallet.address);
+      rareDegenCountBefore = await niftyRareDegenDistribution.getRareDegensCount();
+
+      await niftyDegen.connect(niftyWallet).setApprovalForAll(niftyRareDegenDistribution.address, true);
+      await niftyRareDegenDistribution.connect(niftyWallet).claimRandomRareDegen(degenTokenIdList);
+
+      let niftyWalletDegenCountAfter = await niftyDegen.balanceOf(niftyWallet.address);
+      rareDegenCountAfter = await niftyRareDegenDistribution.getRareDegensCount();
+
+      expect(niftyWalletDegenCountAfter).to.equal(niftyWalletDegenCountBefore.sub(12).add(1));
+      expect(rareDegenCountAfter).to.equal(rareDegenCountBefore.sub(1));
+
+      // Alice claims the random rare degen, burn 8 normal degens to claim 1 rare degen
+      degenTokenIdList = [8, 9, 10, 11, 12, 13, 14, 15];
+
+      aliceDegenCountBefore = await niftyDegen.balanceOf(alice.address);
+      rareDegenCountBefore = await niftyRareDegenDistribution.getRareDegensCount();
+
+      await niftyDegen.connect(alice).setApprovalForAll(niftyRareDegenDistribution.address, true);
+      await niftyRareDegenDistribution.connect(alice).claimRandomRareDegen(degenTokenIdList);
+
+      aliceDegenCountAfter = await niftyDegen.balanceOf(alice.address);
+      rareDegenCountAfter = await niftyRareDegenDistribution.getRareDegensCount();
+
+      expect(aliceDegenCountAfter).to.equal(aliceDegenCountBefore.sub(8).add(1));
+      expect(rareDegenCountAfter).to.equal(rareDegenCountBefore.sub(1));
+    });
+
+    it('Should revert if the user doesn not burn 8 degens', async () => {
+      // Alice claims the random rare degen, burn 8 normal degens to claim 1 rare degen
+      let degenTokenIdList = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+      await niftyDegen.connect(alice).setApprovalForAll(niftyRareDegenDistribution.address, true);
+      await expect(niftyRareDegenDistribution.connect(alice).claimRandomRareDegen(degenTokenIdList)).to.be.revertedWith(
+        'Need 8 degens',
+      );
+    });
+
+    it('Should revert if the nifty wallet doesn not burn 12 degens', async () => {
+      // NiftyWallet claims the random rare degen, burn 12 normal degens to claim 1 rare degen
+      let degenTokenIdList = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+
+      await niftyDegen.connect(niftyWallet).setApprovalForAll(niftyRareDegenDistribution.address, true);
+      await expect(
+        niftyRareDegenDistribution.connect(niftyWallet).claimRandomRareDegen(degenTokenIdList),
+      ).to.be.revertedWith('Need 12 degens');
+    });
+
+    it('Should revert if the contract is paused', async () => {
+      // pause the distribution
+      await niftyRareDegenDistribution.pause();
+
+      // Alice claims the random rare degen, burn 8 normal degens to claim 1 rare degen
+      let degenTokenIdList = [0, 1, 2, 3, 4, 5, 6, 7];
+
+      await niftyDegen.connect(alice).setApprovalForAll(niftyRareDegenDistribution.address, true);
+      await expect(niftyRareDegenDistribution.connect(alice).claimRandomRareDegen(degenTokenIdList)).to.be.reverted;
+    });
+  });
+
+  describe('withdrawAllRareDegens', () => {
+    beforeEach(async () => {
+      const rareDegenTokenIds = [28, 29, 30, 31, 32, 33, 34, 35, 36, 37];
+
+      // deposit the rare degens
+      await niftyDegen.setApprovalForAll(niftyRareDegenDistribution.address, true);
+      await niftyRareDegenDistribution.depositRareDegens(rareDegenTokenIds);
+    });
+
+    it('Should be able to withdraw all rare degens', async () => {
+      const bobRareDegenBalanceBefore = await niftyDegen.balanceOf(bob.address);
+
+      // withdraw all rare degens
+      await niftyRareDegenDistribution.withdrawAllRareDegens(bob.address);
+
+      const bobRareDegenBalanceAfter = await niftyDegen.balanceOf(bob.address);
+
+      expect(bobRareDegenBalanceAfter).to.equal(bobRareDegenBalanceBefore.add(10));
+    });
+
+    it('Should revert if the caller is not the owner', async () => {
+      // withdraw all rare degens
+      await expect(niftyRareDegenDistribution.connect(bob).withdrawAllRareDegens(bob.address)).to.be.reverted;
     });
   });
 
@@ -129,6 +243,11 @@ describe('NiftyRareDegenDistribution', function () {
 
       // check pause status
       expect(await niftyRareDegenDistribution.paused()).to.be.false;
+    });
+
+    it('Should revert if the caller is not the owner', async () => {
+      await expect(niftyRareDegenDistribution.connect(alice).pause()).to.be.reverted;
+      await expect(niftyRareDegenDistribution.connect(alice).unpause()).to.be.reverted;
     });
   });
 });
