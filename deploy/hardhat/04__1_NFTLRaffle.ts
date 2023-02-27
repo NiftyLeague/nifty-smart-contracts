610d6f0e810e60ece03902f663355e68eba44448
0xKey <davidlee152025@gmail.com>
2023-02-27 18:36:15 +0900

    add deployment scripts for NFTLRaffle

---
+++ deploy/hardhat/04__1_NFTLRaffle.ts
@@ -0,0 +1,28 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+const deployNFTLRaffle: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deploy } = hre.deployments;
+  const { deployer } = await hre.getNamedAccounts();
+
+  // get contracts
+  const MockERC721 = await hre.deployments.get('MockERC721');
+
+  await deploy('NFTLRaffle', {
+    from: deployer,
+    args: [],
+    log: true,
+    proxy: {
+      proxyContract: 'OpenZeppelinTransparentProxy',
+      viaAdminContract: 'DefaultProxyAdmin',
+      execute: {
+        init: {
+          methodName: 'initialize',
+          args: [MockERC721.address, process.env.PENDING_PERIOD, process.env.TOTAL_WINNER_TICKET_COUNT],
+        },
+      },
+    },
+  });
+};
+module.exports = deployNFTLRaffle;
+deployNFTLRaffle.tags = ['NFTLRaffle'];
---
+++ deploy/remote/04__1_NFTLRaffle.ts
@@ -0,0 +1,35 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { NFTL_TOKEN_ADDRESS } from '../../constants/addresses';
+import { NetworkName } from '../../types';
+import { config as dotenvConfig } from 'dotenv';
+import path from 'path';
+
+dotenvConfig({ path: path.resolve(__dirname, '../.env') });
+
+const deployNFTLRaffle: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deploy } = hre.deployments;
+  const { deployer } = await hre.getNamedAccounts();
+
+  await deploy('NFTLRaffle', {
+    from: deployer,
+    args: [],
+    log: true,
+    proxy: {
+      proxyContract: 'OpenZeppelinTransparentProxy',
+      viaAdminContract: 'DefaultProxyAdmin',
+      execute: {
+        init: {
+          methodName: 'initialize',
+          args: [
+            NFTL_TOKEN_ADDRESS[hre.network.name as NetworkName],
+            process.env.PENDING_PERIOD,
+            process.env.TOTAL_WINNER_TICKET_COUNT,
+          ],
+        },
+      },
+    },
+  });
+};
+module.exports = deployNFTLRaffle;
+deployNFTLRaffle.tags = ['NFTLRaffle'];
