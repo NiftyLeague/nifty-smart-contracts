57b38237643074b7dfe5520d6ec02ed71aaf6159
0xKey <davidlee152025@gmail.com>
2023-03-06 09:13:10 +0900

    add the deployment script for ledger

---
+++ deploy/ledger/04__1_NFTLRaffle.ts
@@ -0,0 +1,43 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { getLedgerSigner } from '../../scripts/ledger';
+import { NFTL_TOKEN_ADDRESS, VRF_COORDINATOR_ADDRESS } from '../../constants/addresses';
+import { NetworkName } from '../../types';
+import { config as dotenvConfig } from 'dotenv';
+import path from 'path';
+
+dotenvConfig({ path: path.resolve(__dirname, '../.env') });
+
+const deployNFTLRaffle: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deploy } = hre.deployments;
+
+  try {
+    const deployer = await getLedgerSigner();
+
+    await deploy('NFTLRaffle', {
+      from: await deployer.getAddress(),
+      args: [],
+      log: true,
+      proxy: {
+        proxyContract: 'OpenZeppelinTransparentProxy',
+        viaAdminContract: 'DefaultProxyAdmin',
+        execute: {
+          init: {
+            methodName: 'initialize',
+            args: [
+              NFTL_TOKEN_ADDRESS[hre.network.name as NetworkName],
+              process.env.PENDING_PERIOD,
+              process.env.TOTAL_WINNER_TICKET_COUNT,
+              VRF_COORDINATOR_ADDRESS[hre.network.name as NetworkName],
+            ],
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.error('\nFailed in the NFTLRaffle contract deployment using the ledger\n');
+    console.error(err);
+  }
+};
+module.exports = deployNFTLRaffle;
+deployNFTLRaffle.tags = ['NFTLRaffle'];
