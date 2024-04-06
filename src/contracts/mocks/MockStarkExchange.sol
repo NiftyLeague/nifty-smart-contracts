5ee4c5ac88d693e2d9a79c28bba536c37db69c38
NiftyAndy <andy@niftyleague.com>
2024-04-05 22:58:10 -0400

    Update MockStarkExchange

--- src/contracts/mocks/MockStarkExchange.sol
+++ src/contracts/mocks/MockStarkExchange.sol
@@ -3,15 +3,22 @@
 pragma solidity ^0.8.25;
 
 contract MockStarkExchange {
-    constructor() {
-        // Do nothing
-    }
+    mapping(address acc => uint256 balance) private _balances;
+
+    error InsufficientBalance();
+    error DepositAmountMustBeGreaterThanZero();
+    error WithdrawalAmountMustBeGreaterThanZero();
 
     function deposit() external payable {
-        // Do nothing
+        if (msg.value == 0) revert DepositAmountMustBeGreaterThanZero();
+        _balances[msg.sender] += msg.value;
     }
 
-    function withdraw(uint256 amount) external {
-        // Do nothing
+    function withdraw(uint256 amount) external returns (bool success, bytes memory data) {
+        if (amount == 0) revert WithdrawalAmountMustBeGreaterThanZero();
+        if (amount > _balances[msg.sender]) revert InsufficientBalance();
+
+        _balances[msg.sender] -= amount;
+        return msg.sender.call{value: amount}("");
     }
 }
