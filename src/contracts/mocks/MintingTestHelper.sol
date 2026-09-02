de2682ca2b7a42d01066d2fb45062af487a19161
NiftyAndy <andy@niftyleague.com>
2026-09-02 11:59:10 -0400

    fix: format MintingTestHelper.sol to satisfy solhint prettier rule
    Prettier expects multiline formatting for single-argument function.

--- src/contracts/mocks/MintingTestHelper.sol
+++ src/contracts/mocks/MintingTestHelper.sol
@@ -5,7 +5,9 @@ pragma solidity 0.8.19;
 import {Minting} from '../utils/Minting.sol';
 
 contract MintingTestHelper {
-  function split(bytes calldata blob) external pure returns (uint256 tokenId, bytes memory blueprint) {
+  function split(
+    bytes calldata blob
+  ) external pure returns (uint256 tokenId, bytes memory blueprint) {
     return Minting.split(blob);
   }
 }
